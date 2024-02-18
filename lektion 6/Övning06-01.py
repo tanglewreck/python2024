b4d293a66a099db3938b2f128a026d572f0423e6
mier <mier@orion>
2024-02-18 12:14:50 +0100

    - Added #!/usr/bin/env python3 to Övning06-01.py

--- lektion 6/Övning06-01.py
+++ lektion 6/Övning06-01.py
@@ -1,10 +1,14 @@
+#!/usr/bin/env python3
+
 # Rätta till koden så att vi:
 # 1. Skapar en funktion som skriver ut vad användaren skriver in.
 # 2. Kör den skapade funktionen.
 
-def my_func(userinput = "foo"):
+def my_func(userinput="foo"):
     myinput = input("Vad vill du att jag ska säga? ")
-    if not myinput: myinput = userinput
+    if not myinput:
+        myinput = userinput
     print("Jag blev ombedd att säga: ", myinput)
+
 
 my_func()
