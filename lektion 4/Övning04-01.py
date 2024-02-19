6525c0044e03fd9ef0eb292e1999b9b84cd3ac50
meri04 <meri04@network365.se>
2024-02-19 12:21:59 +0100

    La till en list comprehension

--- lektion 4/Övning04-01.py
+++ lektion 4/Övning04-01.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 # Rätta till While-loopen så att den fungerar och skriver ut rader enligt
 # mönstret nedan när programmet körs:
 # 27
@@ -8,6 +10,7 @@
 my_first_int = 20
 my_second_int = 300
 # Ovanstående kod ska inte ändras på.
+mysequence = [x for x in range(20,300,7)]
 
 while my_first_int < my_second_int:
     my_first_int = my_first_int + 7
